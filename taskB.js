6b65a375aa032726d72d89d768b756603ef2eaf8
Sardor93-git <129305269+Sardor93-git@users.noreply.github.com>
2023-04-29 18:01:16 +0900

    to find positive numbers

---
+++ taskB.js
@@ -0,0 +1,11 @@
+let numbers = [2, -4, 3, -6, -9, 5, 12];
+
+let num = [];
+
+numbers.forEach((ele) => {
+  if(ele > 0){
+    num.push(ele);
+  }
+});
+ console.log("Positive raqamli array: ", num);
+    
